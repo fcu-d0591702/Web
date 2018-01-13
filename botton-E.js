de79365c709f02dd336b412e36772c92346667c9
Fcu-D0528060 <32485389+Fcu-D0528060@users.noreply.github.com>
2018-01-13 20:24:57 +0800

    Create botton-E.js

---
+++ botton-E.js
@@ -0,0 +1,116 @@
+
+
+
+
+
+window.onload=function(){
+		
+
+	/*Button-E_1*/	
+		var ButtonE04_1 = document.getElementById("Button-E04_1");
+		var ButtonE05_1 = document.getElementById("Button-E05_1");
+		var ButtonE06_1 = document.getElementById("Button-E06_1");
+		
+		ButtonE05_1.addEventListener("mouseover", function(){		
+			ButtonE04_1.style.opacity = 0.3;
+			ButtonE06_1.style.opacity = 1;
+		});
+			
+		ButtonE05_1.addEventListener("mouseout", function(){
+			ButtonE04_1.style.opacity = 0;
+			ButtonE06_1.style.opacity = 0;
+		});
+
+	/*Button-E_2*/	
+		var ButtonE04_2 = document.getElementById("Button-E04_2");
+		var ButtonE05_2 = document.getElementById("Button-E05_2");
+		var ButtonE06_2 = document.getElementById("Button-E06_2");
+		
+		ButtonE05_2.addEventListener("mouseover", function(){	
+			ButtonE04_2.style.opacity = 0.3;
+			ButtonE06_2.style.opacity = 1;
+		});
+			
+		ButtonE05_2.addEventListener("mouseout", function(){
+			ButtonE04_2.style.opacity = 0;
+			ButtonE06_2.style.opacity = 0;
+		});	
+		
+	/*Button-E_3*/	
+		var ButtonE04_3 = document.getElementById("Button-E04_3");
+		var ButtonE05_3 = document.getElementById("Button-E05_3");
+		var ButtonE06_3 = document.getElementById("Button-E06_3");
+		
+		ButtonE05_3.addEventListener("mouseover", function(){		
+			ButtonE04_3.style.opacity = 0.3;
+			ButtonE06_3.style.opacity = 1;
+		});
+			
+		ButtonE05_3.addEventListener("mouseout", function(){
+			ButtonE04_3.style.opacity = 0;
+			ButtonE06_3.style.opacity = 0;
+		});	
+		
+	/*Button-E_4*/	
+		var ButtonE04_4 = document.getElementById("Button-E04_4");
+		var ButtonE05_4 = document.getElementById("Button-E05_4");
+		var ButtonE06_4 = document.getElementById("Button-E06_4");
+		
+		ButtonE05_4.addEventListener("mouseover", function(){	
+			ButtonE04_4.style.opacity = 0.3;
+			ButtonE06_4.style.opacity = 1;
+		});
+			
+		ButtonE05_4.addEventListener("mouseout", function(){
+			ButtonE04_4.style.opacity = 0;
+			ButtonE06_4.style.opacity = 0;
+		});	
+		
+	/*Button-E_5*/	
+		var ButtonE04_5 = document.getElementById("Button-E04_5");
+		var ButtonE05_5 = document.getElementById("Button-E05_5");
+		var ButtonE06_5 = document.getElementById("Button-E06_5");
+		
+		ButtonE05_5.addEventListener("mouseover", function(){	
+			ButtonE04_5.style.opacity = 0.3;
+			ButtonE06_5.style.opacity = 1;
+		});
+			
+		ButtonE05_5.addEventListener("mouseout", function(){
+			ButtonE04_5.style.opacity = 0;
+			ButtonE06_5.style.opacity = 0;
+		});	
+		
+	/*Button-E_6*/	
+		var ButtonE04_6 = document.getElementById("Button-E04_6");
+		var ButtonE05_6 = document.getElementById("Button-E05_6");
+		var ButtonE06_6 = document.getElementById("Button-E06_6");
+		
+		ButtonE05_6.addEventListener("mouseover", function(){	
+			ButtonE04_6.style.opacity = 0.3;
+			ButtonE06_6.style.opacity = 1;
+		});
+			
+		ButtonE05_6.addEventListener("mouseout", function(){
+			ButtonE04_6.style.opacity = 0;
+			ButtonE06_6.style.opacity = 0;
+		});	
+		
+	/*Button-E_7*/	
+		var ButtonE04_7 = document.getElementById("Button-E04_7");
+		var ButtonE05_7 = document.getElementById("Button-E05_7");
+		var ButtonE06_7 = document.getElementById("Button-E06_7");
+		
+		ButtonE05_7.addEventListener("mouseover", function(){		
+			ButtonE04_7.style.opacity = 0.3;
+			ButtonE06_7.style.opacity = 1;
+		});
+			
+		ButtonE05_7.addEventListener("mouseout", function(){
+			ButtonE04_7.style.opacity = 0;
+			ButtonE06_7.style.opacity = 0;
+		});	
+		
+
+		
+	};
