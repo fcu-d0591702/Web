6c587ef9269f947dd5f9bd588979a41a19633ab9
fcu-d0528073 <32485423+fcu-0528073@users.noreply.github.com>
2018-01-13 20:08:27 +0800

    Create Button-F.js

---
+++ Button-F.js
@@ -0,0 +1,117 @@
+
+
+
+
+
+
+window.onload=function(){
+
+	/*Button-F_1*/	
+		var ButtonF01_1 = document.getElementById("Button-F01_1");
+		var ButtonF02_1 = document.getElementById("Button-F02_1");
+		
+		ButtonF02_1.addEventListener("mouseover", function(){
+			ButtonF01_1.style.backgroundColor = "rgb(246,146,6)";		
+		});
+			
+		ButtonF02_1.addEventListener("mouseout", function(){
+			ButtonF01_1.style.backgroundColor = "rgb(142,183,89)";
+		});
+		
+	/*Button-F_2*/	
+		var ButtonF01_2 = document.getElementById("Button-F01_2");
+		var ButtonF02_2 = document.getElementById("Button-F02_2");
+		
+		ButtonF02_2.addEventListener("mouseover", function(){
+			ButtonF01_2.style.backgroundColor = "rgb(246,146,6)";		
+		});
+			
+		ButtonF02_2.addEventListener("mouseout", function(){
+			ButtonF01_2.style.backgroundColor = "rgb(142,183,89)";
+		});
+		
+	/*Button-F_3*/	
+		var ButtonF01_3 = document.getElementById("Button-F01_3");
+		var ButtonF02_3 = document.getElementById("Button-F02_3");
+		
+		ButtonF02_3.addEventListener("mouseover", function(){
+			ButtonF01_3.style.backgroundColor = "rgb(246,146,6)";		
+		});
+			
+		ButtonF02_3.addEventListener("mouseout", function(){
+			ButtonF01_3.style.backgroundColor = "rgb(142,183,89)";
+		});
+		
+	/*Button-F_4*/	
+		var ButtonF01_4 = document.getElementById("Button-F01_4");
+		var ButtonF02_4 = document.getElementById("Button-F02_4");
+		
+		ButtonF02_4.addEventListener("mouseover", function(){
+			ButtonF01_4.style.backgroundColor = "rgb(246,146,6)";		
+		});
+			
+		ButtonF02_4.addEventListener("mouseout", function(){
+			ButtonF01_4.style.backgroundColor = "rgb(142,183,89)";
+		});
+		
+	/*Button-F_5*/	
+		var ButtonF01_5 = document.getElementById("Button-F01_5");
+		var ButtonF02_5 = document.getElementById("Button-F02_5");
+		
+		ButtonF02_5.addEventListener("mouseover", function(){
+			ButtonF01_5.style.backgroundColor = "rgb(246,146,6)";		
+		});
+			
+		ButtonF02_5.addEventListener("mouseout", function(){
+			ButtonF01_5.style.backgroundColor = "rgb(142,183,89)";
+		});
+		
+	/*Button-F_6*/	
+		var ButtonF01_6 = document.getElementById("Button-F01_6");
+		var ButtonF02_6 = document.getElementById("Button-F02_6");
+		
+		ButtonF02_6.addEventListener("mouseover", function(){
+			ButtonF01_6.style.backgroundColor = "rgb(246,146,6)";		
+		});
+			
+		ButtonF02_6.addEventListener("mouseout", function(){
+			ButtonF01_6.style.backgroundColor = "rgb(142,183,89)";
+		});
+		
+	/*Button-F_7*/	
+		var ButtonF01_7 = document.getElementById("Button-F01_7");
+		var ButtonF02_7 = document.getElementById("Button-F02_7");
+		
+		ButtonF02_7.addEventListener("mouseover", function(){
+			ButtonF01_7.style.backgroundColor = "rgb(246,146,6)";		
+		});
+			
+		ButtonF02_7.addEventListener("mouseout", function(){
+			ButtonF01_7.style.backgroundColor = "rgb(142,183,89)";
+		});
+		
+	/*Button-F_8*/	
+		var ButtonF01_8 = document.getElementById("Button-F01_8");
+		var ButtonF02_8 = document.getElementById("Button-F02_8");
+		
+		ButtonF02_8.addEventListener("mouseover", function(){
+			ButtonF01_8.style.backgroundColor = "rgb(246,146,6)";		
+		});
+			
+		ButtonF02_8.addEventListener("mouseout", function(){
+			ButtonF01_8.style.backgroundColor = "rgb(142,183,89)";
+		});
+		
+	/*Button-F_9*/	
+		var ButtonF01_9 = document.getElementById("Button-F01_9");
+		var ButtonF02_9 = document.getElementById("Button-F02_9");
+		
+		ButtonF02_9.addEventListener("mouseover", function(){
+			ButtonF01_9.style.backgroundColor = "rgb(246,146,6)";		
+		});
+			
+		ButtonF02_9.addEventListener("mouseout", function(){
+			ButtonF01_9.style.backgroundColor = "rgb(142,183,89)";
+		});
+		
+};
